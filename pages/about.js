95b1a287a2cdb015147772ecb94bca23b6b7b0c5
Edy <edyxicon@gmail.com>
2021-12-03 00:31:31 -0600

    primer commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout"
+import styles from "../styles/Layout.module.css"
+
+export default function about() {
+  return (
+    <div className={styles.container}>
+      <Layout
+        title="About | next.js"
+        description="agregue la descripcion"
+      >
+        <h1>about</h1>
+      </Layout>
+    </div>
+  )
+}
